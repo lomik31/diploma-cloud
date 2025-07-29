d152d46e8acfd95ba701d5d4ff5c93aa5850ac1c
lomik31 <lomik31@codead.dev>
2025-07-29 17:03:42 +0300

    nginx container now support multiple domains

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -5,22 +5,26 @@ CERT_DIR="${CERT_DIR:-/etc/nginx/certs}"
 CERT="${CERT_DIR}/fullchain.crt"
 KEY="${CERT_DIR}/private.key"
 DAYS="${DAYS_VALID:-30}"
-SUBJ="/CN=${DOMAIN}"
 
 # Создаём каталог, если он ещё не существует
 mkdir -p "$CERT_DIR"
 
-# Проверяем, есть ли пара файлов
+
 if [ ! -f "$CERT" ] || [ ! -f "$KEY" ]; then
-  echo "⚠️  Сертификаты не найдены — генерирую самоподписанные…"
-  openssl req -x509 -nodes -days "$DAYS" -newkey rsa:4096 \
-         -keyout "$KEY" -out "$CERT" -subj "$SUBJ"
-  echo "✅  Готово: $CERT + $KEY"
+CN=${DOMAINS%%,*}
+SAN="subjectAltName=DNS:$(echo "$DOMAINS" | sed 's/,/,DNS:/g')"
+
+   echo "⚠️  Сертификаты не найдены — генерирую самоподписанные…"
+   openssl req -x509 -nodes -days "$DAYS" -newkey rsa:4096 \
+      -keyout "$KEY" -out "$CERT" \
+      -subj "/CN=${CN}" \
+      -addext "${SAN}"
+   echo "✅  Готово: $CERT + $KEY"
 else
-  echo "🔑  Найдены существующие сертификаты — пропускаю генерацию."
+   echo "🔑  Найдены существующие сертификаты — пропускаю генерацию."
 fi
 
-
-sed -i "s/\\\$DOMAIN\\\$/${DOMAIN}/g" /etc/nginx/nginx.conf
+DOMAINS_SPACED="${DOMAINS//,/ }"
+sed -i "s/\\\$DOMAIN\\\$/${DOMAINS_SPACED}/g" /etc/nginx/nginx.conf
 
 exec "$@"
