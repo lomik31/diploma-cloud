342de41ef860dbe2a304ec9db7b39118c0b6cc1b
lomik31 <lomik31@codead.dev>
2025-08-06 02:51:11 +0300

    Revert "devops fixes"
    This reverts commit 083a24c4077a25a76f21d783b35c834f50f0b965.

--- backend/src/utils/settings.py
+++ backend/src/utils/settings.py
@@ -40,11 +40,6 @@ CSRF_TRUSTED_ORIGINS = [f"https://{i}" for i in env.list(
     default=["localhost"],
 )]
 
-CORS_ALLOWED_ORIGINS = [f"https://{i}" for i in env.list(
-    "DJANGO_ALOWED_HOSTS",
-    default=["localhost"],
-)]
-
 
 # Application definition
 
@@ -61,13 +56,11 @@ INSTALLED_APPS += [
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
-    "corsheaders",
     "users",
     "storage",
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
