31dc004bec16752b61152e92cb1016fa119cdf15
lomik31 <lomik31@codead.dev>
2025-08-09 19:42:45 +0300

    fixes

--- frontend/src/api/files.ts
+++ frontend/src/api/files.ts
@@ -44,7 +44,7 @@ export async function deleteFile(id: number): Promise<void> {
     await api.delete(`/files/${id}/`);
 };
 
-export async function updateFileMeta(id: number, name: string, comment: string): Promise<FileMeta> {
-    const { data } = await api.patch(`/files/${id}/`, { name, comment });
+export async function updateFileMeta(id: number, filename: string, comment: string): Promise<FileMeta> {
+    const { data } = await api.patch(`/files/${id}/`, { filename, comment });
     return data;
 }
