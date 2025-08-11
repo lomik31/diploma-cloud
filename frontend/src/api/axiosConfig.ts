8a829cb9dd57cbb1e399b462a9994cf0ee39210c
lomik31 <lomik31@codead.dev>
2025-08-11 04:43:26 +0300

    frontend: changed the base URL for the API to use the current host.

--- frontend/src/api/axiosConfig.ts
+++ frontend/src/api/axiosConfig.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = import.meta.env.VITE_API_URL;
+const baseURL = `https://${window.location.host}/api/`;
 
 declare module "axios" {
     export interface AxiosRequestConfig {
